7b88d9c96988ef6a47d2758bef9809e261cc2a2e
Ryan Kupka <ryankupk@gmail.com>
2024-10-31 22:59:44 -0600

    Minor performance update

--- submissions/delete-characters-to-make-fancy-string.go
+++ submissions/delete-characters-to-make-fancy-string.go
@@ -7,25 +7,35 @@
 // @lc code=start
 func makeFancyString(s string) string {
 	if len(s) < 3 {
-        return s
-    }
+		return s
+	}
 
-    res := []byte{s[0]}
-    consecutive := 1
+	// res := []byte{s[0]}
+	res := make([]byte, len(s))
+	res[0] = s[0]
+	resIdx := 1
+	addedChars := 1
+	consecutive := 1
 
-    for i := 1; i < len(s); i++ {
-        if s[i] == s[i-1] {
-            consecutive++
-            if consecutive < 3 {
-                res = append(res, s[i])
-            }
-        } else {
-            consecutive = 1
-            res = append(res, s[i])
-        }
-    }
+	for i := 1; i < len(s); i++ {
+		if s[i] == s[i-1] {
+			consecutive++
+			if consecutive < 3 {
+				// res = append(res, s[i])
+				res[resIdx] = s[i]
+				resIdx++
+				addedChars++
+			}
+		} else {
+			consecutive = 1
+			// res = append(res, s[i])
+			res[resIdx] = s[i]
+			resIdx++
+			addedChars++
+		}
+	}
 
-    return string(res)
+	return string(res[:addedChars])
 }
 
 // @lc code=end
